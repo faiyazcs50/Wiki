23a04231d02a486f6d6c8f96aeb28acc87f7aa8b
faiyazcs50 <faiyazcs50@github.com>
2021-01-30 16:32:53 +0530

    Entry Done

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('wiki/<str:name>', views.visit)
+    path('wiki/<str:name>', views.Entry, name="visit")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from . import util
+import os
 
 
 def index(request):
@@ -8,6 +8,10 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def visit(request, name):
-    return render(request, f"encyclopedia/{name}.html")
-
+def Entry(request, name):
+    if util.get_entry(name) is None:
+        return render(request, "encyclopedia/not_found.html",{
+            "name": name
+        })
+    else:
+        return render(request, f"encyclopedia/{name}.html")
